5f83fe9f662a9c71eee8c611d7293c99d4bb9a0a
zhgmaths <576508007@qq.com>
2021-05-13 21:03:03 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,35 @@
+module.exports = {
+	title: "贵哥讲高中数学",
+	descrition: "我的个人博客",
+	themeConfig: {
+		nav: [{
+				text: '首页',
+				link: '/'
+			},
+
+			{
+				text: '高考',
+				link: '/数学/'
+			},
+
+			{
+				text: '百度',
+				link: 'https://baidu.com'
+			},
+		],
+		sidebar: {
+			"/数学/": ["", "公式"],
+		}
+	},
+	plugins: [
+		[
+			'vuepress-plugin-mathjax',
+			{
+				target: 'svg',
+				macros: {
+					'*': '\\times',
+				},
+			},
+		],
+	],
+}
